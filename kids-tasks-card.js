4440fae58a6a6bffdd82d66cb5893a41d404f276
Renaud HACQUIN <git@hacquin.com>
2025-08-15 13:22:16 +0200

    debug

--- kids-tasks-card.js
+++ kids-tasks-card.js
@@ -962,7 +962,7 @@ class KidsTasksCard extends HTMLElement {
       </form>
     `;
 
-    const dialog = this.showModal(content, isEdit ? 'Modifier la tâche' : 'Créer une tâche v0.0');
+    const dialog = this.showModal(content, isEdit ? 'Modifier la tâche' : 'Créer une tâche v0.1');
     
     // Ajouter les event listeners après affichage du modal
     setTimeout(() => {
