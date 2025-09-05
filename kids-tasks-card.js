14a63d360cfe986599c692af712c11e0ee72c0b3
Renaud HACQUIN <git@hacquin.com>
2025-09-05 20:08:53 +0200

    debug

--- kids-tasks-card.js
+++ kids-tasks-card.js
@@ -372,7 +372,7 @@ class KidsTasksStyleManager {
       
       .kt-level-badge--child-card {
         position: absolute;
-        top: 80px;
+        top: 100px;
         background: var(--custom-points-badge-color, var(--primary-color, #3f51b5));
         color: white;
         padding: var(--kt-space-xs) 8px;
